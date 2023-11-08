8d2ddd63df63082441230bba7bb59f8784a90227
João Neto <jflorencioadv@gmail.com>
2023-11-08 22:37:50 +0100

    add.nuevo.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
- // Aquí tu código
+ // Aquí tu código //  no lo entendi bien, perceber la apresentacion del profesor. 
+
+ function addItemToList() {
+    
+    const agregar = prompt('agregar');
+
+    if (agregar) {
+        
+            const listItem = document.createElement('li')
+
+            console.log(agregar)
+
+            listItem.textContent = agregar;
+
+            document.getElementById('agregar').appendChild(listItem)
+            const addButton = document.getElementById('addButton')
